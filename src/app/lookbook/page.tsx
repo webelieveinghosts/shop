2996a0d717bdcb954263ee3113c579786ba5f314
marvinho <twothreeseven237@outlook.com>
2025-04-20 18:29:03 -0300

    update

--- src/app/lookbook/page.tsx
+++ src/app/lookbook/page.tsx
@@ -3,73 +3,73 @@ export default function LookBook() {
         <main className="flex flex-col mx-auto max-w-screen-2xl px-4 space-y-5">
             <div className="flex flex-col space-y-2 md:flex-row md:space-x-2 md:space-y-0">
                 <div>
-                    <img src="/image/lookbook/0.jpg" decoding="async" width="3200" height="4000" />
+                    <img src="/image/lookbook/0.JPG" decoding="async" width="3200" height="4000" />
                 </div>
                 <div>
-                    <img src="/image/lookbook/1.jpg" decoding="async" width="3200" height="4000" />
+                    <img src="/image/lookbook/1.JPG" decoding="async" width="3200" height="4000" />
                 </div>
                 <div>
-                    <img src="/image/lookbook/2.jpg" decoding="async" width="3200" height="4000" />
+                    <img src="/image/lookbook/2.JPG" decoding="async" width="3200" height="4000" />
                 </div>
                 <div>
-                    <img src="/image/lookbook/3.jpg" decoding="async" width="3200" height="4000" />
+                    <img src="/image/lookbook/3.JPG" decoding="async" width="3200" height="4000" />
                 </div>
                 <div>
-                    <img src="/image/lookbook/4.jpg" decoding="async" width="3200" height="4000" />
-                </div>
-            </div>
-
-            <div className="flex flex-col space-y-2 md:flex-row md:space-x-2 md:space-y-0">
-                <div>
-                    <img src="/image/lookbook/5.jpg" decoding="async" width="3200" height="4000" />
-                </div>
-                <div>
-                    <img src="/image/lookbook/6.jpg" decoding="async" width="3200" height="4000" />
-                </div>
-                <div>
-                    <img src="/image/lookbook/7.jpg" decoding="async" width="3200" height="4000" />
-                </div>
-                <div>
-                    <img src="/image/lookbook/8.jpg" decoding="async" width="3200" height="4000" />
-                </div>
-                <div>
-                    <img src="/image/lookbook/9.jpg" decoding="async" width="3200" height="4000" />
+                    <img src="/image/lookbook/4.JPG" decoding="async" width="3200" height="4000" />
                 </div>
             </div>
 
             <div className="flex flex-col space-y-2 md:flex-row md:space-x-2 md:space-y-0">
                 <div>
-                    <img src="/image/lookbook/10.jpg" decoding="async" width="3200" height="4000" />
+                    <img src="/image/lookbook/5.JPG" decoding="async" width="3200" height="4000" />
                 </div>
                 <div>
-                    <img src="/image/lookbook/11.jpg" decoding="async" width="3200" height="4000" />
+                    <img src="/image/lookbook/6.JPG" decoding="async" width="3200" height="4000" />
                 </div>
                 <div>
-                    <img src="/image/lookbook/12.jpg" decoding="async" width="3200" height="4000" />
+                    <img src="/image/lookbook/7.JPG" decoding="async" width="3200" height="4000" />
                 </div>
                 <div>
-                    <img src="/image/lookbook/13.jpg" decoding="async" width="3200" height="4000" />
+                    <img src="/image/lookbook/8.JPG" decoding="async" width="3200" height="4000" />
                 </div>
                 <div>
-                    <img src="/image/lookbook/14.jpg" decoding="async" width="3200" height="4000" />
+                    <img src="/image/lookbook/9.JPG" decoding="async" width="3200" height="4000" />
                 </div>
             </div>
 
             <div className="flex flex-col space-y-2 md:flex-row md:space-x-2 md:space-y-0">
                 <div>
-                    <img src="/image/lookbook/15.jpg" decoding="async" width="3200" height="4000" />
+                    <img src="/image/lookbook/10.JPG" decoding="async" width="3200" height="4000" />
                 </div>
                 <div>
-                    <img src="/image/lookbook/16.jpg" decoding="async" width="3200" height="4000" />
+                    <img src="/image/lookbook/11.JPG" decoding="async" width="3200" height="4000" />
                 </div>
                 <div>
-                    <img src="/image/lookbook/17.jpg" decoding="async" width="3200" height="4000" />
+                    <img src="/image/lookbook/12.JPG" decoding="async" width="3200" height="4000" />
                 </div>
                 <div>
-                    <img src="/image/lookbook/18.jpg" decoding="async" width="3200" height="4000" />
+                    <img src="/image/lookbook/13.JPG" decoding="async" width="3200" height="4000" />
                 </div>
                 <div>
-                    <img src="/image/lookbook/19.jpg" decoding="async" width="3200" height="4000" />
+                    <img src="/image/lookbook/14.JPG" decoding="async" width="3200" height="4000" />
+                </div>
+            </div>
+
+            <div className="flex flex-col space-y-2 md:flex-row md:space-x-2 md:space-y-0">
+                <div>
+                    <img src="/image/lookbook/15.JPG" decoding="async" width="3200" height="4000" />
+                </div>
+                <div>
+                    <img src="/image/lookbook/16.JPG" decoding="async" width="3200" height="4000" />
+                </div>
+                <div>
+                    <img src="/image/lookbook/17.JPG" decoding="async" width="3200" height="4000" />
+                </div>
+                <div>
+                    <img src="/image/lookbook/18.JPG" decoding="async" width="3200" height="4000" />
+                </div>
+                <div>
+                    <img src="/image/lookbook/19.JPG" decoding="async" width="3200" height="4000" />
                 </div>
             </div>
         </main>
--- src/components/pages/product/product-gallery.tsx
+++ src/components/pages/product/product-gallery.tsx
@@ -8,10 +8,10 @@ export const ProductGallery = ({ id, images }: { id: number, images: string[] })
     return (
         <div className="flex flex-row gap-2">
             <div className="flex flex-col gap-2 w-16 max-sm:w-14 shrink-0">
-                {images.map((target, index) => <img src={`https://lhlpxtxqdlctohptywpi.supabase.co/storage/v1/object/public/products/${id}/${target}`} alt={`Image-${index}`} key={`Image-${index}`} className={`aspect-[64/85] object-cover object-top w-full cursor-pointer  border-b-2 ${image === index ? "border-primary" : "border-transparent"}`} onClick={() => image !== index && setImage(index)} />)}
+                {images.map((target, index) => <img src={`https://lhlpxtxqdlctohptywpi.supabase.co/storage/v1/object/public/products/${id}/${target}`} alt={`Image-${index}`} key={`Image-${index}`} className={`aspect-[1/1] object-cover object-top w-full cursor-pointer  border-b-2 ${image === index ? "border-primary" : "border-transparent"}`} onClick={() => image !== index && setImage(index)} />)}
             </div>
             <div className="flex-1">
-                {images.map((target, index) => <img src={`https://lhlpxtxqdlctohptywpi.supabase.co/storage/v1/object/public/products/${id}/${target}`} alt={`Image-${index}`} key={`Image-${index}`} className={`w-full aspect-[548/712] object-cover ${image !== index && "hidden"}`} />)}
+                {images.map((target, index) => <img src={`https://lhlpxtxqdlctohptywpi.supabase.co/storage/v1/object/public/products/${id}/${target}`} alt={`Image-${index}`} key={`Image-${index}`} className={`w-full aspect-[1/1] object-cover ${image !== index && "hidden"}`} />)}
             </div>
         </div>
     )
