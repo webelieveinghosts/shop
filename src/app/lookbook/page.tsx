a501f36f65044282ace34b1956e06bd66b5bb60c
marvinho <twothreeseven237@outlook.com>
2025-04-21 13:47:42 -0300

    update lookbook

--- src/app/lookbook/page.tsx
+++ src/app/lookbook/page.tsx
@@ -3,73 +3,73 @@ export default function LookBook() {
         <main className="flex flex-col mx-auto max-w-screen-2xl px-4 space-y-5">
             <div className="flex flex-col space-y-2 md:flex-row md:space-x-2 md:space-y-0">
                 <div>
-                    <img src="/image/lookbook/0.JPG" decoding="async" width="3200" height="4000" />
+                    <img src="/image/lookbook/0.webp" decoding="async" width="3200" height="4000" />
                 </div>
                 <div>
-                    <img src="/image/lookbook/1.JPG" decoding="async" width="3200" height="4000" />
+                    <img src="/image/lookbook/1.webp" decoding="async" width="3200" height="4000" />
                 </div>
                 <div>
-                    <img src="/image/lookbook/2.JPG" decoding="async" width="3200" height="4000" />
+                    <img src="/image/lookbook/2.webp" decoding="async" width="3200" height="4000" />
                 </div>
                 <div>
-                    <img src="/image/lookbook/3.JPG" decoding="async" width="3200" height="4000" />
+                    <img src="/image/lookbook/3.webp" decoding="async" width="3200" height="4000" />
                 </div>
                 <div>
-                    <img src="/image/lookbook/4.JPG" decoding="async" width="3200" height="4000" />
-                </div>
-            </div>
-
-            <div className="flex flex-col space-y-2 md:flex-row md:space-x-2 md:space-y-0">
-                <div>
-                    <img src="/image/lookbook/5.JPG" decoding="async" width="3200" height="4000" />
-                </div>
-                <div>
-                    <img src="/image/lookbook/6.JPG" decoding="async" width="3200" height="4000" />
-                </div>
-                <div>
-                    <img src="/image/lookbook/7.JPG" decoding="async" width="3200" height="4000" />
-                </div>
-                <div>
-                    <img src="/image/lookbook/8.JPG" decoding="async" width="3200" height="4000" />
-                </div>
-                <div>
-                    <img src="/image/lookbook/9.JPG" decoding="async" width="3200" height="4000" />
+                    <img src="/image/lookbook/4.webp" decoding="async" width="3200" height="4000" />
                 </div>
             </div>
 
             <div className="flex flex-col space-y-2 md:flex-row md:space-x-2 md:space-y-0">
                 <div>
-                    <img src="/image/lookbook/10.JPG" decoding="async" width="3200" height="4000" />
+                    <img src="/image/lookbook/5.webp" decoding="async" width="3200" height="4000" />
                 </div>
                 <div>
-                    <img src="/image/lookbook/11.JPG" decoding="async" width="3200" height="4000" />
+                    <img src="/image/lookbook/6.webp" decoding="async" width="3200" height="4000" />
                 </div>
                 <div>
-                    <img src="/image/lookbook/12.JPG" decoding="async" width="3200" height="4000" />
+                    <img src="/image/lookbook/7.webp" decoding="async" width="3200" height="4000" />
                 </div>
                 <div>
-                    <img src="/image/lookbook/13.jpg" decoding="async" width="3200" height="4000" />
+                    <img src="/image/lookbook/8.webp" decoding="async" width="3200" height="4000" />
                 </div>
                 <div>
-                    <img src="/image/lookbook/14.jpg" decoding="async" width="3200" height="4000" />
+                    <img src="/image/lookbook/9.webp" decoding="async" width="3200" height="4000" />
                 </div>
             </div>
 
             <div className="flex flex-col space-y-2 md:flex-row md:space-x-2 md:space-y-0">
                 <div>
-                    <img src="/image/lookbook/15.jpg" decoding="async" width="3200" height="4000" />
+                    <img src="/image/lookbook/10.webp" decoding="async" width="3200" height="4000" />
                 </div>
                 <div>
-                    <img src="/image/lookbook/16.jpg" decoding="async" width="3200" height="4000" />
+                    <img src="/image/lookbook/11.webp" decoding="async" width="3200" height="4000" />
                 </div>
                 <div>
-                    <img src="/image/lookbook/17.jpg" decoding="async" width="3200" height="4000" />
+                    <img src="/image/lookbook/12.webp" decoding="async" width="3200" height="4000" />
                 </div>
                 <div>
-                    <img src="/image/lookbook/18.jpg" decoding="async" width="3200" height="4000" />
+                    <img src="/image/lookbook/13.webp" decoding="async" width="3200" height="4000" />
                 </div>
                 <div>
-                    <img src="/image/lookbook/19.jpg" decoding="async" width="3200" height="4000" />
+                    <img src="/image/lookbook/14.webp" decoding="async" width="3200" height="4000" />
+                </div>
+            </div>
+
+            <div className="flex flex-col space-y-2 md:flex-row md:space-x-2 md:space-y-0">
+                <div>
+                    <img src="/image/lookbook/15.webp" decoding="async" width="3200" height="4000" />
+                </div>
+                <div>
+                    <img src="/image/lookbook/16.webp" decoding="async" width="3200" height="4000" />
+                </div>
+                <div>
+                    <img src="/image/lookbook/17.webp" decoding="async" width="3200" height="4000" />
+                </div>
+                <div>
+                    <img src="/image/lookbook/18.webp" decoding="async" width="3200" height="4000" />
+                </div>
+                <div>
+                    <img src="/image/lookbook/19.webp" decoding="async" width="3200" height="4000" />
                 </div>
             </div>
         </main>
