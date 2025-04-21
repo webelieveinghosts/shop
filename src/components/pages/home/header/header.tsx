6ab5c5f467a1761cdd077b762327780826dd9c70
marvinho <twothreeseven237@outlook.com>
2025-04-21 13:15:52 -0300

    optimize banners

--- src/components/pages/home/header/header.tsx
+++ src/components/pages/home/header/header.tsx
@@ -9,10 +9,10 @@ export const Header = ({ mobile }: { mobile: boolean }) => {
             <div className="md:col-span-4 md:row-span-2">
                 {mobile ?
                     <div className="group flex h-full w-full items-center justify-center overflow-hidden rounded-lg border border-gray-400 hover:border-primary">
-                        <img fetchPriority="high" decoding="async" loading="lazy" className="relative h-full w-full object-cover transition duration-300 ease-in-out group-hover:scale-105" src="/image/slide/banner-mobile.png" />
+                        <img fetchPriority="high" decoding="async" loading="lazy" className="relative h-full w-full object-cover transition duration-300 ease-in-out group-hover:scale-105" src="/image/slide/banner-mobile.webp" />
                     </div> :
                     <div className="group flex items-center justify-center overflow-hidden rounded-lg border border-gray-400 hover:border-primary">
-                        <img fetchPriority="high" decoding="async" loading="lazy" className="relative w-full transition duration-300 ease-in-out group-hover:scale-105" src="/image/slide/banner-pc.png" />
+                        <img fetchPriority="high" decoding="async" loading="lazy" className="relative w-full transition duration-300 ease-in-out group-hover:scale-105" src="/image/slide/banner-pc.webp" />
                     </div>
                 }
             </div>
