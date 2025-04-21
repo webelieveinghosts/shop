b9bdd86c49a95be3fec71dd336a6e3c7bdb6709f
marvinho <twothreeseven237@outlook.com>
2025-04-21 13:11:04 -0300

    mobile logo

--- src/components/ui/navbar/navbar.tsx
+++ src/components/ui/navbar/navbar.tsx
@@ -4,15 +4,9 @@ import { CartModal } from "../cart/cart-modal"
 export const Navbar = () => {
     return (
         <nav className="relative flex items-center justify-between p-4 lg:px-6">
-            <div className="block flex-none md:hidden">
-                <button className="flex h-11 w-11 items-center justify-center text-black transition-colors md:hidden">
-                    <Logo />
-                </button>
-            </div>
-
             <div className="flex w-full items-center">
-                <div className="flex w-full md:w-1/3">
-                    <ul className="hidden gap-6 text-xs md:flex md:items-center uppercase">
+                <div className="hidden md:flex w-1/3">
+                    <ul className="gap-6 text-xs md:flex md:items-center uppercase">
                         <li>
                             <a className="text-black/60 underline-offset-4 hover:text-black hover:underline transition-all duration-300" href="/">Início</a>
                         </li>
@@ -22,8 +16,8 @@ export const Navbar = () => {
                     </ul>
                 </div>
 
-                <div className="hidden justify-center md:flex md:w-1/3">
-                    <a className="mr-2 flex w-full items-center justify-center md:w-auto lg:mr-6" href="/">
+                <div className="flex w-full md:justify-center md:w-1/3">
+                    <a className="mr-2 flex w-full items-center md:justify-center md:w-auto lg:mr-6" href="/">
                         <Logo className="w-14" />
                     </a>
                 </div>
