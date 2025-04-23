9cd651e78a9f396d7da462b5db6f83f0e4c484b3
marvinho <twothreeseven237@outlook.com>
2025-04-23 18:09:09 -0300

    Update actions.tsx

--- src/components/ui/cart/actions.tsx
+++ src/components/ui/cart/actions.tsx
@@ -3,6 +3,7 @@
 import { addToCart, removeFromCart } from "@/supabase/cart/client"
 import { Database } from "@/supabase/database"
 import { cookies } from "next/headers"
+import { redirect } from "next/navigation"
 
 export const addItem = async (prevState: any, { productId, size }: { productId: number, size?: Database["public"]["Enums"]["size"] }) => {
     const cartId = await cookies().then(cookies => cookies.get("cartId")?.value)
@@ -28,6 +29,6 @@ export const removeItem = async (prevState: any, productId: number) => {
 
 
 export const redirectToCheckout = async () => {
-    // let cart = await getCart()
-    // redirect(cart!.checkoutUrl)
+    const cartId = await cookies().then(cookies => cookies.get("cartId")?.value)
+    redirect(`https://checkout.webelieveinghosts.com.br/${cartId}`)
 }
