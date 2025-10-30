0f54d109397e12c4251d56971fc8de88f207df0e
haxbash <bluemaquinajp@gmail.com>
2025-10-30 05:57:17 -0300

    chore: Format footer component for consistency

--- src/components/ui/footer/footer.tsx
+++ src/components/ui/footer/footer.tsx
@@ -3,14 +3,16 @@ import { Logo } from "../logo/logo"
 export const Footer = () => {
     return (
         <footer className="bg-white min-w-full">
-            <div className="w-full  p-4 pb-6 lg:pb-8 pt-4 lg:pt-5">
+            <div className="w-full p-4 pb-6 lg:pb-8 pt-4 lg:pt-5">
                 <div className="md:flex md:justify-between pt-4 lg:pt-5 border-t border-zinc-300">
                     <div className="flex flex-col mb-6 md:mb-0 justify-between">
                         <a href="#" className="flex items-center">
                             <Logo className="h-8" />
                         </a>
 
-                        <span className="text-sm text-gray-500 sm:text-center">© 2025 <a href="#" className="hover:underline">WBG Store</a>. All Rights Reserved.</span>
+                        <span className="text-sm text-gray-500 sm:text-center">
+                            © 2025 <a href="#" className="hover:underline">WBG Store</a>. All Rights Reserved.
+                        </span>
                     </div>
                     <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
                         {/*
@@ -30,7 +32,7 @@ export const Footer = () => {
                             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Social</h2>
                             <ul className="text-gray-500 font-medium">
                                 <li className="mb-4">
-                                    <a href="https://www.instagram.com/webelieveinghosts/" className="hover:underline ">Instagram</a>
+                                    <a href="https://www.instagram.com/webelieveinghosts/" className="hover:underline">Instagram</a>
                                 </li>
                             </ul>
                         </div>
@@ -46,7 +48,7 @@ export const Footer = () => {
                                 </li>
                             </ul>
                         </div>
-                         */}
+                        */}
                     </div>
                 </div>
             </div>
