7333f77cef4df6ca304b13ac96db1bc9d747f503
haxbash <bluemaquinajp@gmail.com>
2025-10-30 06:25:54 -0300

    refactor: Remove hover effect from header image container

--- src/components/pages/home/header/header.tsx
+++ src/components/pages/home/header/header.tsx
@@ -15,12 +15,12 @@ export const Header = () => {
     return mobile !== undefined && (
         <section className="flex items-center justify-center w-full min-h-[300px] md:min-h-[500px] px-4">
             <div className="w-full max-w-screen-2xl">
-                <div className="group flex items-center justify-center overflow-hidden rounded-lg border border-gray-400 hover:border-primary">
+                <div className="flex items-center justify-center overflow-hidden rounded-lg border border-gray-400">
                     <img
                         fetchPriority="high"
                         decoding="async"
                         loading="lazy"
-                        className="w-full h-auto md:h-[500px] object-cover transition-transform duration-300 ease-in-out group-hover:scale-105"
+                        className="w-full h-auto md:h-[500px] object-cover"
                         src={mobile ? "/image/slide/banner-mobile.webp" : "/image/slide/banner-pc.webp"}
                     />
                 </div>
