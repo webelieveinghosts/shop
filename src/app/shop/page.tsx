f8370b12ca71585190edf8b608a94c54fcbabe37
haxbash <bluemaquinajp@gmail.com>
2025-11-03 04:54:35 -0300

    feat: Add basic shop page layout

---
+++ src/app/shop/page.tsx
@@ -0,0 +1,9 @@
+export default function Shop() {
+    return (
+        <main className="flex flex-col mx-auto max-w-screen-2xl px-4 space-y-5">
+            <div className="flex flex-col space-y-2 md:flex-row md:space-x-2 md:space-y-0">
+                <h1>Shop</h1>
+            </div>
+        </main>
+    )
+}
