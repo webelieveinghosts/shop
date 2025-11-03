7d017d71a4acbe047193c1337d0b19123ec62bdb
haxbash <bluemaquinajp@gmail.com>
2025-11-03 05:01:16 -0300

    feat: Display categories on the shop page

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,9 +1,16 @@
-export default function Shop() {
+import { Category } from "@/components/pages/home/categories/category"
+import { getCategories } from "@/supabase/queries"
+import { createClient } from "@/supabase/server"
+
+export default async function Home() {
+    const supabase = await createClient()
+    const categories = await getCategories(supabase)
+
     return (
-        <main className="flex flex-col mx-auto max-w-screen-2xl px-4 space-y-5">
-            <div className="flex flex-col space-y-2 md:flex-row md:space-x-2 md:space-y-0">
-                <h1>Shop</h1>
-            </div>
-        </main>
+        <div className="w-full space-y-5">
+
+            {categories.map((value, index) => <Category key={`category-${index}`} category={value} />)}
+
+        </div>
     )
 }
